41de3de6e775cf336838f3d22dc3ab7808cac289
cynthia <cyn.torres.leal@gmail.com>
2026-01-18 02:36:59 -0300

    Creación de pantallas

---
+++ app/src/main/java/com/example/expressgourmet/ui/screens/SplashScreen.kt
@@ -0,0 +1,139 @@
+package com.example.expressgourmet.ui.screens
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.example.expressgourmet.R
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.expressgourmet.ui.theme.Orange
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreen(){
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ){
+        Image(
+            painter = painterResource(R.drawable.fondo), // Carga una IMAGEN desde los recursos
+            contentDescription = "Fondo",
+            modifier = Modifier.fillMaxSize(), // Ocupa todo el ancho y el alto de la pantalla
+            contentScale = ContentScale.Crop, // Crop hace que la IMAGEN cubra toda la pantalla
+            colorFilter = ColorFilter.tint(
+                color = Color.Black.copy(alpha = 0.4f),
+                blendMode = BlendMode.Darken
+            )
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 80.dp, start = 20.dp, end = 20.dp, bottom = 80.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Column {
+                Text(
+                    text = "Express",
+                    color = Color.White,
+                    fontSize = 65.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(
+                    text = "Gourmet",
+                    color = Orange,
+                    fontSize = 55.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Text(
+                text = "Recetas saludables para tu día a día.",
+                modifier = Modifier.semantics{
+                    contentDescription = "Eslogan: Recetas saludables para tu día a día."
+                },
+                color = Color.White,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Button(
+                onClick = { /* Acción */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(65.dp),
+                shape = RoundedCornerShape(20.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Orange)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "COMENZAR",
+                        color = Color.White,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "¿Ya tienes cuenta?",
+                    color = Color.White,
+                    fontSize = 20.sp
+                )
+
+                Spacer(Modifier.width(width = 15.dp))
+
+                Text(
+                    text = "Inicia sesión",
+                    color = Orange,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
